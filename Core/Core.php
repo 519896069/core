92a303bbe86667a0ec39248bb7a6ac85858a28cc
fzp <519896069@qq.com>
2019-03-04 15:45:53 +0800

    project init

--- Core/Core.php
+++ Core/Core.php
@@ -28,7 +28,22 @@ class Core
     public static function routers()
     {
         Route::namespace(self::$base_namespace)
-            ->group('Core/Components/Routes/router.php');
+            ->group(function () {
+                Route::middleware(['transaction'])->group(function () {
+                    Route::get('login', 'AuthAdminController@login')->name('login');
+
+                    Route::middleware(['authenticate'])->group(function () {
+                        Route::resource('user', 'UserAdminController')->only(['index', 'store', 'update', 'show']);
+                        Route::resource('role', 'RoleAdminController')->only(['index', 'store', 'update']);
+                        Route::resource('permission', 'PermissionAdminController')->only(['index', 'store', 'update']);
+
+                        Route::post('roleEmpowerment/{id}', 'PermissionAdminController@roleEmpowerment')->name('role.empowerment');
+                        Route::post('userEmpowerment/{id}', 'PermissionAdminController@userEmpowerment')->name('user.empowerment');
+                        Route::post('syncRoles/{id}', 'PermissionAdminController@syncRoles')->name('sync.roles');
+                        Route::post('logout', 'AuthAdminController@logout')->name('logout');
+                    });
+                });
+            });
     }
 
     /**
