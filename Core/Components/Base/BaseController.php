6fea2800b12ce1df0cbf061712cb7b790d017f56
fzp <519896069@qq.com>
2019-03-05 16:54:58 +0800

    project init

--- Core/Components/Base/BaseController.php
+++ Core/Components/Base/BaseController.php
@@ -2,10 +2,15 @@
 
 namespace Core\Components\Controllers\Base;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller;
 
 class BaseController extends Controller
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
     protected $guard = '';
 
     public function __construct()
