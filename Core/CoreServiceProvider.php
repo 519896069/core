23facee7bb301adec9f484ef4e1a619104e416c2
fzp <519896069@qq.com>
2019-04-12 15:00:41 +0800

    project init

--- Core/CoreServiceProvider.php
+++ Core/CoreServiceProvider.php
@@ -14,20 +14,39 @@ class CoreServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        $this->bind();
+        $this->load();
+        $this->publish();
+    }
+
+    private function publish()
+    {
+        $this->publishes([
+            __DIR__ . '/Components/Config/core.php' => config_path('core.php'),
+        ]);
+
+    }
+
+    private function bind()
+    {
         app()->bind(Websocket::class, WebsocketClient::class);
+
+    }
+
+    private function load()
+    {
         $this->loadMigrationsFrom(__DIR__ . '/../Databases/migrations');
         $this->mergeConfigFrom(__DIR__ . '/Components/Config/core.php', 'core');
 
-        if (config('core.route')) {
-            Core::routers();
-        }
-
         if ($this->app->runningInConsole()) {
-
             $this->commands([
                 CreateAdminUser::class,
                 SocketServer::class,
             ]);
+        }
+
+        if (config('core.route', false)) {
+            Core::routers();
         }
     }
 }
