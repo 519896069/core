77310a508344528b926025a8960497643a2db2c5
fzp <519896069@qq.com>
2019-05-28 00:58:55 +0800

    project init

--- src/Components/Base/AdminController.php
+++ src/Components/Base/AdminController.php
@@ -6,4 +6,61 @@ class AdminController extends BaseController
 {
     protected $guard = 'admin-api';
 
+    private $model_class;
+    /** @type Model */
+    private $model;
+    private $resource_class;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->model = app($this->model_class);
+    }
+
+    public function index()
+    {
+        $list = $this->model->filter(request()->input())->paginateFilter();
+        return ($this->resource_class)::collection($list);
+    }
+
+    public function store()
+    {
+        $data = request($this->model->getFillable());
+        $data = $this->storeData($data);
+        return ($this->resource_class)::make(
+            $this->model->newQuery()->create($data)
+        );
+    }
+
+    public function update($id)
+    {
+        $data = request($this->model->getFillable());
+        $data = $this->updateData($data);
+        return ($this->resource_class)::make(
+            $this->model->newQuery()->whereId($id)->update($data)
+        );
+    }
+
+    public function show($id)
+    {
+        return ($this->resource_class)::make(
+            $this->model->whereId($id)->first()
+        );
+    }
+
+    public function delete($id)
+    {
+        $model = $this->model->newQuery()->findOrFail($id);
+        $model->delete();
+    }
+
+    protected function storeData($data)
+    {
+        return $data;
+    }
+
+    protected function updateData($data)
+    {
+        return $data;
+    }
 }
